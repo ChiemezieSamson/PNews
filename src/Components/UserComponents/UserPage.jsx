38bc048fe00cec4ebf233bfadd05118a2d5caca3
Nebeolisa chiemezie Samson <daddygreenarrow44@gmail.com>
2023-08-20 01:31:00 +0900

    userPage.jsx modified and labled

--- src/Components/UserComponents/UserPage.jsx
+++ src/Components/UserComponents/UserPage.jsx
@@ -4,12 +4,18 @@ import { useWindowSize } from '../SharedAsset/SharedAssets';
 import MyProfilePicture from './UserPageComponents/MyProfilePicture';
 
 const UserPage = () => {
+  const [navlinkTextContene, setNavlinkTextContene] = useState("My Profile")
   const size = useWindowSize()
-  const [navlinkTextContene, setNavlinkTextContene] = useState("")
 
   const handleNavlinkTextContene = (e) => {
     setNavlinkTextContene(() => e.target.textContent)
   }
+
+  // when on small screen open the profile picture only on my profile nav
+  let hideshowProfilePicture 
+  if (size.width < 1316 && navlinkTextContene === "My Profile") { hideshowProfilePicture = true }
+  // on lager screen always keep open
+  if (size.width >= 1316) {hideshowProfilePicture = true}
 
   const UserPages = [
     {
@@ -24,46 +30,47 @@ const UserPage = () => {
     },
     {
       id: 3,
-      name: "Sign in & security",
+      name: "Sign in & Security",
       toUrl: "/userpage/security",
+    },
+    {
+      id: 4,
+      name: "General",
+      toUrl: "/userpage/General",
     },
   ];  
 
-  let hideshowProfilePicture 
-  if (size.width < 1316 && navlinkTextContene === "My Profile") { hideshowProfilePicture = true }
-  if (size.width >= 1316) {hideshowProfilePicture = true}
+  const style = `border-l-[10px] border-solid bg-gray-200/40 hover:bg-neutral-200 mt-2.5 mb-2.5 px-3 text-stone-800 TextHeadertransition cursor-pointer block py-2 h-[50px] sm:h-auto tracking-wider text-sm sm:text-base`
 
-  const style = `border-l-[10px] border-solid bg-[#eee] mt-2.5 mb-2.5 px-3 text-[#282a35] transition-all 
-  duration-200 ease-linear cursor-pointer block py-2 h-[50px] sm:h-auto tracking-wider text-sm sm:text-base`
-
+  
   return (
     <div className={`${size.width >= 1316 ? "grid grid-cols-4 gap-x-4" : "grid grid-flow-row "} font-poppins`}>
 
-      <div className={`mt-6 text-left  
-      ${size.width >= 1316 ? "col-span-1" : "order-first grid grid-flow-col justify-evenly"}`}>
-        {UserPages.map((page) => {
+      {/* user navigation list */}
+      <div className={`mt-6 text-left ${size.width >= 1316 ? "col-span-1 max-h-56" : "order-first grid grid-flow-col justify-evenly"}`}>
+
+        {UserPages?.map((page) => {
             return (
-              <div key={page.id} className = "pb-7 sm:mt-2 sm:mb-4">
-                <NavLink 
-                to={page.toUrl} end 
-                className={({ isActive }) => isActive ? `${style} border-[#f70d28] font-semibold` : style}
-                onClick={handleNavlinkTextContene}
-                >{page.name}</NavLink>
+              <div key={page.id} className = "mb-7 sm:mt-2 sm:mb-4">
+                <NavLink to={page.toUrl} end className={({ isActive }) => isActive ? `${style} border-[#f70d28] font-semibold` : style}
+                 onClick={handleNavlinkTextContene}>{page.name}</NavLink>
               </div>              
             )
           })
         }                
       </div>
 
+      {/* user information content */}
       <div className={`${size.width >= 1316 ? "col-span-2" : "order-last"} bg-gray-200/40 overflow-x-hidden rounded-md`}>
         <Outlet/>
       </div>
-
+      
+      {/* user image */}
       <div className={`${hideshowProfilePicture ? "block" : "hidden"}`}>
         <div className={`${size.width >= 1316 ? "col-span-1" : "order-2"}`}>
-         <MyProfilePicture />
-        </div>
-      </div>            
+          <MyProfilePicture />
+        </div>         
+      </div>
     </div>
   )
 }
