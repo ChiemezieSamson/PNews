7a4429627e45a376c8e66cfff530862a1e5c3fef
Nebeolisa chiemezie Samson <daddygreenarrow44@gmail.com>
2023-08-28 22:36:17 +0900

    Modified the BusinessIndexPage.jsx

--- src/Components/BlogPages/businessPage/BusinessIndexPage.jsx
+++ src/Components/BlogPages/businessPage/BusinessIndexPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import StickyBox from "react-sticky-box";
-import { AdminComponent, CategoriesComponent, NavDirectionAndPageName, overLay, TimeComponent } from "../../SharedAsset/SharedAssets";
+import { AdminComponent, CategoriesComponent, isFecthingStyle, NavDirectionAndPageName, overLay, TimeComponent } from "../../SharedAsset/SharedAssets";
 import Aside from "../asidePage/Aside";
 import { PagesBlogPostComponent } from "../IndexPageComponents/SharedComponents";
 import { Link } from "react-router-dom";
@@ -22,38 +22,39 @@ const BusinessIndexPage = () => {
   const canOpen = [action, useraction, commentaction].every(Boolean)
  
   return (
-    <div className='disabled:opacity-40' disabled={isFetching}>
+    <div  className={`${isFecthingStyle(isFetching)}`}>
 
       {canOpen ?
         <ul className="hidden md:grid md:gap-1 md:grid-rows-6 md:grid-cols-7 mb-4 md:max-h-[450px] lg:max-h-[500px]">
+
           {Posts?.slice(0, 4)?.map((post) => {
             return (
-              <li key={post._id} className="md:first:col-start-1 md:first:col-end-4 md:first:row-start-1 md:first:row-end-7 
+              <li key={post?._id} className="md:first:col-start-1 md:first:col-end-4 md:first:row-start-1 md:first:row-end-7 
                   md:[&:nth-child(2)]:col-start-4 md:[&:nth-child(2)]:col-end-6 md:[&:nth-child(2)]:row-start-1 md:[&:nth-child(2)]:row-end-7  
                   md:[&:nth-child(3)]:col-start-6 md:[&:nth-child(3)]:col-end-8 md:[&:nth-child(3)]:row-start-1 md:[&:nth-child(3)]:row-end-4   
                   md:last:col-start-6 md:last:col-end-8 md:last:row-start-4 md:last:row-end-7 relative group">
 
-                <Link to={`/single/${post._id}`}  className={overLay()}>
-                  <img src={publicFolder + post.postImage} alt="post"  loading="lazy"/>
+                <Link to={`/single/${post?._id}`}  className={overLay()}>
+                  <img src={publicFolder + post?.postImage} alt="post"  loading="lazy"/>
                 </Link>
 
                 <span className="text-white absolute -top-[7px] left-0 z-30 max-w-fit inset-x-auto">
-                  <CategoriesComponent cat={post.postCategory[0]} />
+                  <CategoriesComponent cat={post?.postCategory[0]} />
                 </span>                    
 
-                <Link to={`/single/${post._id}`} className="absolute group-first:bottom-[8%] group-[&:nth-child(2)]:bottom-[8%] bottom-[12%] max-w-fit text-white inset-x-auto z-30 mx-1">
+                <Link to={`/single/${post?._id}`} className="absolute group-first:bottom-[8%] group-[&:nth-child(2)]:bottom-[8%] bottom-[12%] max-w-fit text-white inset-x-auto z-30 mx-1">
                     <h4 className="group-first:text-lg group-first:lg:text-2xl HeroImageMultipleListH3" title='title'>
                       <span>
-                        {post.postTitle}
+                        {post?.postTitle}
                       </span>
                     </h4>
                 </Link>
 
-                <span className='absolute bottom-0 max-w-fit text-white right-0 z-30 mx-1 backdrop-blur-sm bg-black/10 bg-blend-burn px-px '>
+                <span className='absolute bottom-0 max-w-fit text-white right-0 z-30 mx-1 rounded-md group-hover:backdrop-blur-sm group-hover:bg-black/10 group-hover:bg-blend-burn px-px'>
                   <span className="inline-block mr-4 hover:mainColor TextHeadertransition"> 
                     <AdminComponent user={post?.postAuthor} users={users}/>
                   </span>
-                  <TimeComponent time={post.createdAt}/>
+                  <TimeComponent time={post?.createdAt}/>
                 </span> 
               </li>
             )
@@ -70,20 +71,20 @@ const BusinessIndexPage = () => {
        />
       }
 
-        <div className="md:hidden">
-          <SharedBlogPageStyleOne 
-            Posts={canOpen && Posts?.slice(1, 4)} 
-            users={users}
-            canOpen={canOpen}
-          />
-        </div>
+      <div className="md:hidden">
+        <SharedBlogPageStyleOne 
+          Posts={canOpen && Posts?.slice(1, 4)} 
+          users={users}
+          canOpen={canOpen}
+        />
+      </div>
 
 
       <div className='grid grid-flow-row md:grid-cols-3'>
 
         <aside className="md:col-span-1 mt-8 md:mr-[3%] order-last md:order-first">
           <StickyBox offsetTop={0} offsetBottom={0}>
-            <Aside Comments={Comments}/>
+            <Aside Comments={Comments} commentaction={commentaction}/>
           </StickyBox>
         </aside>
 
