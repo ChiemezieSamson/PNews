630ed7a7dc3f4e3d0f72a45b948a59097b9f892d
Nebeolisa chiemezie Samson <daddygreenarrow44@gmail.com>
2023-09-12 19:28:15 +0900

    made changes to the ports

--- src/Reduxstore/store/store.js
+++ src/Reduxstore/store/store.js
@@ -17,6 +17,7 @@ const store = configureStore({
 	},
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware().concat(apiSlice.middleware),
+	devTools: process.env.NODE_ENV !== "production",
 });
 
 export default store;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider as Rrouters } from "react-router-dom";
 import store from "./Reduxstore/store/store";
 import { Provider } from "react-redux";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import "./index.css";
 import App from "./App";
+
+if (process.env.NODE_ENV === "production") {
+	disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
