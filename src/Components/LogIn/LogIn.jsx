ef6888e78ed29853d32400c4104c3fe3609c475f
Nebeolisa chiemezie Samson <daddygreenarrow44@gmail.com>
2023-09-28 17:42:40 +0900

    handle the error notification in login page

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -9,7 +9,7 @@ import { CorrectTick, PasswordDisplay, isFecthingStyle } from '../SharedAsset/Sh
 
 const LogIn = () => {
   // Redux toolkit that calls the back end for a log in
-  const [login, { isLoading, isSuccess }] = useLoginMutation()
+  const [login, { isLoading, isSuccess, isError, error }] = useLoginMutation()
 
   const [username, setUsername] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
@@ -99,15 +99,15 @@ const LogIn = () => {
         if (!err?.originalStatus) {
           
           // isLoading: true until timeout occurs
-          setErrMsg('No Server Response');
+          setErrMsg(error.data);
           setErrMsgOn(() => true)
         } else if (err.originalStatus === 400) {
 
           setErrMsg('fill all the input form');
           setErrMsgOn(() => true)
-        } else if (err.originalStatus === 401) {
+        } else if (err.originalStatus === 401 || isError) {
 
-          setErrMsg('Unauthorized');
+          setErrMsg(error.data);
           setErrMsgOn(() => true)
         } else {
 
